03a34d5a32f854c43e1fd6cf4c6f1bbcd305bb37
invencaosts <gui.inven@gmail.com>
2025-04-12 13:37:45 -0300

    fix Instalando efeito de typing

--- invencaodev/app/page.tsx
+++ invencaodev/app/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { TypeAnimation } from "react-type-animation";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
       <div id="home">
-        <h1 className="text-4xl font-bold text-gray-800">
-          Welcome to Invencaodev
-        </h1>
+        <p>Oi, meu nome é</p>
+        <p>Guilherme da Invenção</p>
+        <TypeAnimation
+          sequence={[
+            "Desenvolvedor Full-Stack",
+            500,
+            "Criador de soluções em software",
+            900,
+            "Apaixonado por tecnologia",
+            400,
+          ]}
+          repeat={Infinity}
+          speed={99}
+        />
       </div>
       <Footer />
     </div>
