c3fe623f4a9304260e4ab13679a15ebf1131ba9e
invencaosts <gui.inven@gmail.com>
2025-04-13 14:20:03 -0300

    fix Resolvendo bug da fonte

--- invencaodev/app/layout.tsx
+++ invencaodev/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Arvo } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const arvo = Arvo({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${arvo.className} antialiased`}>{children}</body>
     </html>
   );
 }
