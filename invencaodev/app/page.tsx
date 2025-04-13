7ab321232bc310dfa72a104971c1eab5313188ba
invencaosts <gui.inven@gmail.com>
2025-04-13 14:10:07 -0300

    fix Responsividade home e ajeitando fonte

--- invencaodev/app/page.tsx
+++ invencaodev/app/page.tsx
@@ -7,12 +7,14 @@ import Button from "@/components/Button";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="font-arvo">
       <Header />
       <div id="home" className="p-4 text-secundary">
-        <div className="font-bold">
-          <p className="text-xl">Oi, meu nome é</p>
-          <p className="text-4xl text-primary">Guilherme da Invenção</p>
+        <div className="font-bold overflow-hidden">
+          <p className="text-xl md:text-2xl">Oi, meu nome é</p>
+          <p className="text-4xl text-primary md:text-6xl">
+            Guilherme da Invenção
+          </p>
           <TypeAnimation
             sequence={[
               "Desenvolvedor Full-Stack",
@@ -24,19 +26,19 @@ const Home: React.FC = () => {
             ]}
             repeat={Infinity}
             speed={50}
-            className="text-2xl"
+            className="text-2xl md:text-4xl"
           />
         </div>
         <div>
-          <p className="mt-3">
+          <p className="mt-3 md:text-xl md:w-[50%]">
             Bem-vindo! Sou um desenvolvedor de software com experiência em
             desenvolvimento web. Comecei minha jornada na programação em 2021 e
             sou apaixonado por novas tecnologias. Estou sempre buscando aprender
             e me aprimorar a cada dia.
           </p>
         </div>
-        <div>
-          <div className="mt-4">
+        <div className="md:flex md:w-full md:justify-between md:gap-4">
+          <div className="mt-4 md:w-[50%]">
             <Button
               label="Ver Cúrriculo"
               size="md"
@@ -48,7 +50,7 @@ const Home: React.FC = () => {
               href="https://drive.google.com/file/d/1UEqj05hKkTWgyKCU0T_nIwsj4kMdqcBT/view?usp=drive_link"
             />
           </div>
-          <div className="mt-4">
+          <div className="mt-4 md:w-[50%]">
             <Button
               label="Entre em contato"
               size="md"
